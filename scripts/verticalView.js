21137d6ca5ebadb79050a82c78a500b6063f7b04
Trevor Eyre <trevoreyre@gmail.com>
2019-08-13 13:49:46 +0000

    Remove background video color overlay on button hover

--- scripts/verticalView.js
+++ scripts/verticalView.js
@@ -1,1 +1,12 @@
 console.log('verticalView.js');
+
+const videoBackground = document.querySelector('.sqs-video-background')
+const tourButton = document.querySelector(".sqs-block-button-element[href='/the-gym']")
+console.log({ videoBackground, tourButton })
+
+tourButton.addEventListener('mouseenter', () => {
+  videoBackground.classList.add('active')
+})
+tourButton.addEventListener('mouseleave', () => {
+  videoBackground.classList.remove('active')
+})
